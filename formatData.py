5547c9a2a15974df7200e711c544cf7e00683a7b
Impossiblu <56997847+Impossiblu@users.noreply.github.com>
2020-02-05 16:39:48 +0000

    Updated so null values are now replaced with appropriate values

--- formatData.py
+++ formatData.py
@@ -1,9 +1,5 @@
 import pandas as pd
-import math
-
-removeRowsWithNull = [] #any houses that have a null value for attributes in this array will be removed from calculations
-#Placed here since I'm not sure how this is populated
-#The only other place where this is refered to is reading values from it - Laurence
+import numpy as np
 
 quantVars = ['OverallQual','FullBath','BedroomAbvGr','KitchenAbvGr','TotRmsAbvGrd','Fireplaces','YearRemodAdd','GarageCars','YrSold','GrLivArea','GarageArea','TotalBsmtSF'] # values are a number or null
 qualVars = ['ExterQual','HeatingQC','KitchenQual','GarageQual','ExterCond'] # values are Ex Gd TA Fa Po or null
@@ -16,14 +12,39 @@ totalVars = quantVars + qualVars + yesNoVars + yesNoNumberVars + otherVars
 def GetTotalVars():
     return totalVars
 
+def FormatNulls(dataset):
+    #List of columns where null values indicate 'None'
+    colNone = ['Alley', 'MasVnrType', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 'PoolQC', 'Fence', 'MiscFeature', 'KitchenQual']
+    
+    #List of columns where nulls are replaced with the most common value
+    colMostCommon = ['Electrical', 'MSZoning', 'Utilities', 'Exterior1st', 'Exterior2nd', 'Functional', 'SaleType']
+    
+    #List of columns where nulls are replaced with 0
+    colZero = ['MasVnrArea', 'GarageYrBlt', 'BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 'BsmtFullBath', 'BsmtHalfBath', 'GarageCars', 'GarageArea']
+    
+    for col in colNone:
+        dataset[col] = dataset[col].fillna("None")
+        
+    for col in colMostCommon:
+        dataset[col] = dataset[col].fillna(dataset[col].value_counts().idxmax())
+        
+    for col in colZero:
+        dataset[col] = dataset[col].fillna(0)
+    
+    #Fill nulls based on LotFrontage values of houses in the same neighborhood
+    for i in dataset.index:
+        if (np.isnan(dataset.loc[i, 'LotFrontage'])):
+            dataset.loc[i, 'LotFrontage'] = dataset.groupby('Neighborhood')['LotFrontage'].median()[dataset.loc[i, 'Neighborhood']]
+    
+    return dataset
+
 def FormatQual(column):
     column = column.replace("Ex", value="5")
     column = column.replace("Gd", value="4")
     column = column.replace("TA", value="3")
     column = column.replace("Fa", value="2")
     column = column.replace("Po", value="1")
-    #column = column.replace("NA", value="0")
-    #column = column.replace("nan", value="0")
+    column = column.replace("None", value="0")
     return column
 
 def FormatSlope(column):
@@ -38,9 +59,8 @@ def FormatYesNo(column):
     column = column.replace("N", value="0")
     return column
 
-def FormatData(dataset, removeNulls=False):
-    for quant in quantVars: # fills all quant null vars with median
-        dataset[quant] = dataset[quant].fillna(math.floor(dataset[quant].median()))
+def FormatData(dataset, removeAbnormals=False):
+    dataset = FormatNulls(dataset)
     
     for qual in qualVars: # changes all qual values to numbers and fills in nulls
         dataset[qual] = FormatQual(dataset[qual])
@@ -57,12 +77,7 @@ def FormatData(dataset, removeNulls=False):
     #other vars formatting
     dataset['LandSlope'] = FormatSlope(dataset['LandSlope'])
     
-    if removeNulls == True: #Remove houses with null attributes or abnormal sale condiditon
-        #removes all values with null values in certain columns
-        for column in removeRowsWithNull:
-            dataset = dataset[pd.notnull(dataset[column])]
-        
-        #Removes all houses with an abnormal sale condition
+    if removeAbnormals == True: #Remove houses with abnormal sale condiditon
         indexNames = dataset[dataset['SaleCondition'] == "Abnorml" ].index
         dataset.drop(indexNames, inplace=True)
         
