fa6727d313d9bce83dd71d41e07ec65f757144fe
Impossiblu <56997847+Impossiblu@users.noreply.github.com>
2020-02-21 09:16:27 +0000

    Updated to accomodate changes to formatData.py

--- Main.py
+++ Main.py
@@ -2,9 +2,7 @@ import pandas as pd
 import math
 from polynomialRegression import PolynomialRegression
 from linearRegression import LinearRegression
-from formatData import FormatData, GetTotalVars
-
-totalVars = GetTotalVars()  #Variables used when training models and predicting
+from formatData import FormatData, FillMissingDummies, GetTotalVars
 
 #Reads the training data from the csv file and allocates it to a dataframe called ds
 ds = pd.read_csv("https://raw.githubusercontent.com/Impossiblu/HousingPricesPrediction/master/train.csv")
@@ -16,22 +14,28 @@ test_df = pd.read_csv("https://raw.githubusercontent.com/Impossiblu/HousingPrice
 #Format test data
 test_df = FormatData(test_df)
 
+#Ensures both datasets have the same columns
+ds, test_df = FillMissingDummies(ds, test_df)
+
+#Gets list of variables used to predict results
+totalVars = GetTotalVars(test_df)
 
 #Linear regression
 linearReg_df = pd.DataFrame()
 
 linearReg_df['SalePrice'] = LinearRegression(ds[totalVars], ds.SalePrice, test_df[totalVars]).astype(int).tolist()
 #above line creates the predictions of all the values within the test_dataframe(test_df)
+
+#Gives predictions correct Id and outputs results
 linearReg_df = linearReg_df.set_index(test_df['Id'])
+linearReg_df.to_csv(path_or_buf="submission.csv", index=True)
 
-
+"""
 #Polynomial regression
 polyReg_df = pd.DataFrame()
 
-polyDegree = 2  #Temp value - Have to decide what value to use properly. Higher values take exponentially more time to process and do not work well with current data formatting
+polyDegree = 2  #Temp value - Have to decide what value to use properly. Higher values take exponentially more time and memory to process and do not work well with current data formatting
 
 polyReg_df['SalePrice'] = PolynomialRegression(ds[totalVars], ds.SalePrice, polyDegree, test_df[totalVars]).astype(int).tolist()
 #above line creates the predictions of all the values within the test_dataframe(test_df)
-polyReg_df = polyReg_df.set_index(test_df['Id'])
-
-#No output in this version as there are multiple sets of predictions that need to be combined somehow
+"""
