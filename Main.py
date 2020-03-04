e238cfbdd1b3c7160953a8a5636becb11d3ba1c0
Impossiblu <56997847+Impossiblu@users.noreply.github.com>
2020-03-04 17:45:43 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,37 +1,46 @@
 import pandas as pd
-import math
-from polynomialRegression import PolynomialRegression
-from linearRegression import LinearRegression
-from formatData import FormatData, GetTotalVars
+import numpy as np
+from formatData import FormatData, FillMissingDummies, GetTotalVars
+from modelling import Predict
 
-totalVars = GetTotalVars()  #Variables used when training models and predicting
+import warnings
+def ignore_warn(*args, **kwargs):
+    pass
+warnings.warn = ignore_warn
 
-#Reads the training data from the csv file and allocates it to a dataframe called ds
-ds = pd.read_csv("https://raw.githubusercontent.com/Impossiblu/HousingPricesPrediction/master/train.csv")
-#Format train data with removal of rows with null values
-ds = FormatData(ds, True)
+def rmsle(real, predicted):
+    sum=0.0
+    for x in range(len(predicted)):
+        p = np.log(predicted[x]+1)
+        r = np.log(real[x]+1)
+        sum = sum + (p - r)**2
+    return (sum/len(predicted))**0.5
 
-#Import and format test data
-test_df = pd.read_csv("https://raw.githubusercontent.com/Impossiblu/HousingPricesPrediction/master/test.csv")
-#Format test data
-test_df = FormatData(test_df)
+def OutputPrediction(prediction, ids):
+    outDf = pd.DataFrame()
+    
+    outDf['SalePrice'] = prediction.astype(int).tolist()
+    
+    outDf = outDf.set_index(ids)
+    
+    outDf.to_csv('submission.csv')
+    
+    linr = rmsle(pd.read_csv("train.csv")['SalePrice'].values, outDf['SalePrice'].values)
+    print("score: " + str(linr))
 
-
-#Linear regression
-linearReg_df = pd.DataFrame()
-
-linearReg_df['SalePrice'] = LinearRegression(ds[totalVars], ds.SalePrice, test_df[totalVars]).astype(int).tolist()
-#above line creates the predictions of all the values within the test_dataframe(test_df)
-linearReg_df = linearReg_df.set_index(test_df['Id'])
-
-
-#Polynomial regression
-polyReg_df = pd.DataFrame()
-
-polyDegree = 2  #Temp value - Have to decide what value to use properly. Higher values take exponentially more time to process and do not work well with current data formatting
-
-polyReg_df['SalePrice'] = PolynomialRegression(ds[totalVars], ds.SalePrice, polyDegree, test_df[totalVars]).astype(int).tolist()
-#above line creates the predictions of all the values within the test_dataframe(test_df)
-polyReg_df = polyReg_df.set_index(test_df['Id'])
-
-#No output in this version as there are multiple sets of predictions that need to be combined somehow
+def MainPredict(trainFileName, testFileName):
+    train = pd.read_csv(trainFileName)
+    test = pd.read_csv(testFileName)
+    
+    train = FormatData(train, True)
+    
+    test = FormatData(test)
+    
+    train, test = FillMissingDummies(train, test)
+    
+    totalVars = GetTotalVars(test)
+    
+    OutputPrediction(Predict(train[totalVars], train.SalePrice, test[totalVars]), test['Id'])
+ 
+#Used only when running from this file, comment out when running from gui.py
+#MainPredict("https://raw.githubusercontent.com/Impossiblu/HousingPricesPrediction/master/train.csv", "https://raw.githubusercontent.com/Impossiblu/HousingPricesPrediction/master/test.csv")
